e7580f3eededa6453d5116d11bc1e0315f728545
margaritaboiko <margo250494@gmail.com>
2025-12-28 11:43:12 +0100

    Images webp

--- src/utils/doctors.data.js
+++ src/utils/doctors.data.js
@@ -1,7 +1,7 @@
-import profile1 from '../Assets/profile-1.png'
-import profile2 from '../Assets/profile-2.png'
-import profile3 from '../Assets/profile-3.png'
-import profile4 from '../Assets/profile-4.png'
+import profile1 from '../Assets/profile-1.webp'
+import profile2 from '../Assets/profile-2.webp'
+import profile3 from '../Assets/profile-3.webp'
+import profile4 from '../Assets/profile-4.webp'
 
 export const infoDoctors = [
   {
